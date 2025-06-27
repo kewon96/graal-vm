ebe1b987d1ade28d2ef09c3a1c20dac53ee1ca7d
Seung Hyeon <yang0184@naver.com>
2025-06-27 20:23:34 +0900

    initial commit

---
+++ src/test/java/co/kr/graal_vm/GraalVmApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.graal_vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalVmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
